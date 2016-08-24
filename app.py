313f19bfdfa125f47b4ba9939b8514c02b74061f
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-08-24 18:42:41 +0700

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import fbchat
-
-client = fbchat.Client("khiemhuynh95@gmail.com", "Xindungquayroi95")
-friends = client.getUsers("Khiem Huynh")  # return a list of names
-friend = friends[0]
-sent = client.send(friend.uid, "Your Message")
-if sent:
-    print("Message sent successfully!")
