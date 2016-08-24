32d69122e21efea24d977ccd00ad6b2147efebde
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-08-24 18:37:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import fbchat
 
-client = fbchat.Client("khiemhuynh952@gmail.com", "emyeuquy")
+client = fbchat.Client("khiemhuynh95@gmail.com", "Xindungquayroi95")
 friends = client.getUsers("Khiem Huynh")  # return a list of names
 friend = friends[0]
 sent = client.send(friend.uid, "Your Message")
